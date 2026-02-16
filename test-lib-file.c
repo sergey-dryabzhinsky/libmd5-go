8269652ac519612f13b24eacd128e9fcc8a423d3
Sergey Dryabzhinsky <sergey.dryabzhinsky+github@gmail.com>
2026-02-16 20:17:22 +0300

    Make test read file name via argument

--- test-lib-file.c
+++ test-lib-file.c
@@ -6,9 +6,16 @@ int main(int argc,char **argv){
   printf("libmd5-go version: %s\n", libmd5_go__version());
   printf("go runtime version used: %s\n", libmd5_go__version_go());
   char* dgst;
+  printf("input args number:%d",argc);
+if (argc>1){
+  printf("md5-file(%s): ...\n",argv[1]);
+  dgst = libmd5_go__MD5File_hexdigest(argv[1]);
+  printf("md5-file(%s):%s\n",argv[1], dgst);
+} else {
   printf("md5-file(LICENSE): ...\n");
   dgst = libmd5_go__MD5File_hexdigest("LICENSE");
   printf("md5-file(LICENSE):%s\n", dgst);
+}
 
   printf("md5-file(non-existent): ...\n");
   dgst = libmd5_go__MD5File_hexdigest("non-existent");
